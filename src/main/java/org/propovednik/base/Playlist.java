b21ff3534c6f3d949d42940073d13c4e73255913
Artur Sentsov <sen4ik@gmail.com>
2018-04-21 21:20:26 -0700

    Refactoring to add page objects and come up with architecture

--- src/main/java/org/propovednik/base/Playlist.java
+++ src/main/java/org/propovednik/base/Playlist.java
@@ -1,4 +1,20 @@
 package org.propovednik.base;
 
 public class Playlist {
+
+    public void getPlaylistItems(){
+
+    }
+
+    public void isPlaylistEmpty(){
+
+    }
+
+    public void isResetButtonPresent(){
+
+    }
+
+    public void clickResetButton(){
+
+    }
 }
