6068ac9cd2423ebba6531ca717561e3f2e23ea43
Artur Sentsov <sen4ik@gmail.com>
2018-04-21 21:05:27 -0700

    Refactoring to add page objects and come up with architecture

---
+++ src/main/java/org/propovednik/base/DriverUtility.java
@@ -0,0 +1,21 @@
+package org.propovednik.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DriverUtility {
+
+    public void isElementPresent(By by){
+
+    }
+
+    public void isElementPresent(WebElement element){
+
+    }
+
+    public void isElementPresentWithinAnotherElement(WebElement elementToLookIn, By by){
+
+    }
+
+}
