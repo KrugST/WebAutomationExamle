2610e72cc85ad9cf01440cd957880c84509e65ce
krug_st <krugSTS@gmail.com>
2018-05-30 17:24:31 -0700

    Back to automation, lazy ass, working on TODOs.

--- src/main/java/org/propovednik/admin/AdminLoginPage.java
+++ src/main/java/org/propovednik/admin/AdminLoginPage.java
@@ -7,6 +7,10 @@ public class AdminLoginPage {
     WebDriver driver;
     String login = "rsentsov";
     String password = "Sacramento#2018";
+    private By adminLoginError = By.xpath("//div[@id='msgDiv']/div[contains(text(),'The username/email or password you entered is incorrect.')]");
+    private By loginButtonLocator = By.xpath("//div[@id='login-container-form']//input[@value='Log in']");
+    private By usernameFieldLocator = By.xpath("//div[@id='login-container-form']//input[@name='username']");
+    private By passwordFieldLocator = By.xpath("//div[@id='login-container-form']//input[@name='password']");
 
     public AdminLoginPage(WebDriver driver) {
         this.driver = driver;
@@ -15,23 +19,22 @@ public class AdminLoginPage {
     public void goToAdminLogin() {
         driver.get("https://dev.propovednik.com/admin/");
     }
-    private By adminLoginError = By.xpath("//div[@id='msgDiv']/div[contains(text(),'The username/email or password you entered is incorrect.')]");
 
     public void loginWithAdminAccount() {
         // TODO: refactor locators and similar lines
         goToAdminLogin();
-        driver.findElement(By.xpath("//div[@id='login-container-form']//input[@name='username']")).sendKeys(login);
-        driver.findElement(By.xpath("//div[@id='login-container-form']//input[@name='password']")).sendKeys(password);
-        driver.findElement(By.xpath("//div[@id='login-container-form']//input[@value='Log in']")).click();
+        driver.findElement(usernameFieldLocator).sendKeys(login);
+        driver.findElement(passwordFieldLocator).sendKeys(password);
+        driver.findElement(loginButtonLocator).click();
         driver.findElement(By.xpath("//div[@id='title-div-left']/h1[contains(text(),'Dashboard')]"));
     }
 
     public void loginWithProvidedCredentials(String loginToInput, String passwordToInput) {
         // TODO: refactor locators and similar lines
         goToAdminLogin();
-        driver.findElement(By.xpath("//div[@id='login-container-form']//input[@name='username']")).sendKeys(loginToInput);
-        driver.findElement(By.xpath("//div[@id='login-container-form']//input[@name='password']")).sendKeys(passwordToInput);
-        driver.findElement(By.xpath("//div[@id='login-container-form']//input[@value='Log in']")).click();
+        driver.findElement(usernameFieldLocator).sendKeys(loginToInput);
+        driver.findElement(passwordFieldLocator).sendKeys(passwordToInput);
+        driver.findElement(loginButtonLocator).click();
     }
 
     public void isLoginErrorPresent() {
